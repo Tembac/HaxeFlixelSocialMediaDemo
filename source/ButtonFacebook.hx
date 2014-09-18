23ea52b1dc84c7a1ee38456648e14bbd4008488c
Tembac <agustinpf@gmail.com>
2014-09-17 22:44:18 -0300

    fixed links

--- source/ButtonFacebook.hx
+++ source/ButtonFacebook.hx
@@ -15,15 +15,15 @@ import openfl.Lib;
 class ButtonFacebook extends FlxButton
 {
 	public var appID:String = "508343469312204"; // you get this number from making an app on facebook. Very easy to do. This number ifs for my app PEWMA
-	public var messageTitle:String = "I got a Score of 1000 on the game PEWMA! Can you beat me?";
-	public var messageCaption:String = "PEWMA mobile game";
+	public var messageTitle:String = "I%20got%20a%20Score%20of%201000%20on%20the%20game%20PEWMA!%20Can%20you%20beat%20me?";
+	public var messageCaption:String = "PEWMA%20mobile%20game";
 	public var shareLink:String = "http://chimangogames.com/";
-	public var messageDescription:String = "PEWMA is a game for ios and Android devices! Adictive, fun and inspired on ancient south American cultures. From Chimango Games!";
+	public var messageDescription:String = "PEWMA%20is%20a%20game%20for%20ios%20and%20Android%20devices!%20Adictive,%20fun%20and%20inspired%20on%20ancient%20south%20American%20cultures.%20From%20Chimango%20Games!";
 	public var messageImage:String = "https://dl.dropboxusercontent.com/u/24695185/MobileGames/images/haxeflixel_pewma.jpg";
 	
 	public function new(X:Float=0, Y:Float=0) 
 	{
-		super(X, Y, sendMessage);
+		super(X, Y,"", sendMessage);
 		
 		loadGraphic("assets/images/FB-f-Logo__blue_72.png");
 	}
@@ -41,7 +41,7 @@ class ButtonFacebook extends FlxButton
 		+ "&description=" + messageDescription 
 		+ "&redirect_uri=https://www.facebook.com/";
 		
-		Lib.getURL(new URLRequest(textMessage));
+		Lib.getURL(new URLRequest(textMessage), "_blank");
 	}
 	
 }
--- source/ButtonTwitter.hx
+++ source/ButtonTwitter.hx
@@ -4,6 +4,7 @@ import flash.net.URLRequest;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.ui.FlxButton;
+import flixel.ui.FlxTypedButton;
 import flixel.util.FlxPoint;
 import flixel.util.FlxRect;
 import openfl.Lib;
@@ -14,11 +15,11 @@ import openfl.Lib;
  */
 class ButtonTwitter extends FlxButton
 {
-	public var tweetMessage:String = "Demo from @tembachi on how to use twitter with @haxeflixel Tweet to thank! @chimangogames";
+	public var tweetMessage:String = "Demo%20from%20@tembachi%20on%20how%20to%20use%20twitter%20with%20@haxeflixel%20Tweet%20to%20thank!%20@chimangogames";
 	
-	public function new(X:Float=0, Y:Float=0) 
+	public function new(X:Float=0, Y:Float=0, ?OnClick:Void->Void) 
 	{
-		super(X, Y, sendMessage);
+		super(X, Y,"", sendMessage);//, sendMessage);
 		
 		loadGraphic("assets/images/Twitter_logo_blue_72.png");
 	}
@@ -26,7 +27,7 @@ class ButtonTwitter extends FlxButton
 	private function sendMessage():Void
 	{
 		var textMessage:String = "http://twitter.com/intent/tweet?text=" + tweetMessage;
-		Lib.getURL(new URLRequest(textMessage));
+		Lib.getURL(new URLRequest(textMessage), "_blank");
 	}
 	
 }
